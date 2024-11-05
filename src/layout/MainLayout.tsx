a0aafeeba406ca33d2f260c2823e3491a8d1b643
SiriusShift <lagmanmarquez@gmail.com>
2024-11-05 16:52:59 +0800

    fix layout in dashboard

--- src/layout/MainLayout.tsx
+++ src/layout/MainLayout.tsx
@@ -7,7 +7,7 @@ function MainLayout() {
   return (
     <div className="flex">
       <Sidebar />
-      <div className="flex flex-col lg:ms-80 mx-10 w-[90%] xl:w-[76%]">
+      <div className="flex flex-col lg:ms-80 mx-10 w-full">
         <Header />
         <div className="h-full py-5">
           <Outlet />
--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -107,105 +107,109 @@ const Dashboard = () => {
   };
 
   return (
-    <div className="flex flex-col gap-5">
-      <div className="flex items-center justify-between">
-        <h1 className="text-4xl lg:text-2xl font-bold">Dashboard</h1>
-        <Popover>
-          <PopoverTrigger asChild>
-            <Button
-              id="date"
-              variant={"outline"}
-              className={cn(
-                "w-[250px] justify-start text-left font-normal",
-                !date && "text-muted-foreground"
-              )}
-            >
-              <CalendarIcon className="mr-2" />
-              {date?.from ? (
-                date.to ? (
-                  <>
-                    {format(date.from, "LLL dd, y")} -{" "}
-                    {format(date.to, "LLL dd, y")}
-                  </>
-                ) : (
-                  format(date.from, "LLL dd, y")
-                )
-              ) : (
-                <span className="text-lg lg:text-base">Pick a date</span>
-              )}
-            </Button>
-          </PopoverTrigger>
-          <PopoverContent className="w-auto p-0" align="end">
-            <Calendar
-              initialFocus
-              mode="range"
-              defaultMonth={date?.from}
-              selected={date}
-              onSelect={setDate}
-              numberOfMonths={2}
-            />
-          </PopoverContent>
-        </Popover>
-      </div>
-      <div className="grid grid-cols-4 md:grid-rows-1 lg:grid-rows-1 gap-5">
-        <div className="border rounded-lg col-span-2 md:col-span-2 xl:col-span-1 h-44 lg:h-36 border-gray-100 shadow-md"></div>
-        <div className="border rounded-lg col-span-2 md:col-span-2 xl:col-span-1 h-44 lg:h-36 border-gray-100 shadow-md"></div>
-        <div className="border rounded-lg col-span-2 md:col-span-2 xl:col-span-1 h-44 lg:h-36 border-gray-100 shadow-md"></div>
-        <div className="border rounded-lg col-span-2 md:col-span-2 xl:col-span-1 h-44 lg:h-36 border-gray-100 shadow-md"></div>
-      </div>
-      <div className="grid-rows-2 gap-5 lg:grid-rows-1 grid grid-cols-3">
-        <Card className="col-span-4  lg:col-span-2 border rounded-lg border-gray-100 shadow-md">
-          <div className="mx-6 mt-6">
-            <h1 className="text-2xl xl:text-xl font-bold">Active Loans</h1>
-            <CardDescription className="text-xl lg:text-base">
-              Your next payment of ₱10,500 is scheduled for October 30, 2024
-            </CardDescription>
-          </div>
-          <CardContent>
-            <ChartContainer
-              className="h-[300px] lg:h-[250px] mt-5 w-full"
-              config={chartConfig}
-            >
-              <BarChart
-                data={chartData}
-                layout="vertical"
-                margin={{
-                  right: 16,
-                }}
+    <div className="2xl:grid-cols-4 gap-5 grid 2xl:grid-rows-1">
+      <div className="2xl:col-span-3 flex flex-col gap-5">
+        <div className="flex items-center justify-between">
+          <h1 className="text-4xl lg:text-2xl font-bold">Dashboard</h1>
+          <Popover>
+            <PopoverTrigger asChild>
+              <Button
+                id="date"
+                variant={"outline"}
+                className={cn(
+                  "w-[250px] justify-start text-left font-normal",
+                  !date && "text-muted-foreground"
+                )}
               >
-                <CartesianGrid horizontal={false} />
-                <YAxis
-                  dataKey="loanType"
-                  type="category"
-                  tickLine={false}
-                  axisLine={false}
-                />
-                <XAxis
-                  dataKey="paidPercentage"
-                  type="number"
-                  domain={[0, 100]}
-                  tickFormatter={(value) => `${value}%`}
-                />
-                <ChartTooltip cursor={false} content={<CustomTooltip />} />
-                <Bar
-                  dataKey="paidPercentage"
-                  fill="var(--color-desktop)"
-                  radius={4}
+                <CalendarIcon className="mr-2" />
+                {date?.from ? (
+                  date.to ? (
+                    <>
+                      {format(date.from, "LLL dd, y")} -{" "}
+                      {format(date.to, "LLL dd, y")}
+                    </>
+                  ) : (
+                    format(date.from, "LLL dd, y")
+                  )
+                ) : (
+                  <span className="text-lg lg:text-base">Pick a date</span>
+                )}
+              </Button>
+            </PopoverTrigger>
+            <PopoverContent className="w-auto p-0" align="end">
+              <Calendar
+                initialFocus
+                mode="range"
+                defaultMonth={date?.from}
+                selected={date}
+                onSelect={setDate}
+                numberOfMonths={2}
+              />
+            </PopoverContent>
+          </Popover>
+        </div>
+        <div className="grid grid-cols-4 md:grid-rows-1 lg:grid-rows-1 gap-5">
+          <div className="border rounded-lg col-span-2 md:col-span-2 xl:col-span-1 h-44 border-gray-100 shadow-md"></div>
+          <div className="border rounded-lg col-span-2 md:col-span-2 xl:col-span-1 h-44 border-gray-100 shadow-md"></div>
+          <div className="border rounded-lg col-span-2 md:col-span-2 xl:col-span-1 h-44 border-gray-100 shadow-md"></div>
+          <div className="border rounded-lg col-span-2 md:col-span-2 xl:col-span-1 h-44 border-gray-100 shadow-md"></div>
+        </div>
+        <div className="grid-rows-2 gap-5 lg:grid-rows-1 grid grid-cols-3">
+          <Card className="col-span-4  lg:col-span-3 border rounded-lg border-gray-100 shadow-md">
+            <div className="mx-6 mt-6">
+              <h1 className="text-2xl xl:text-xl font-bold">Active Loans</h1>
+              <CardDescription className="text-xl lg:text-base">
+                Your next payment of ₱10,500 is scheduled for October 30, 2024
+              </CardDescription>
+            </div>
+            <CardContent>
+              <ChartContainer
+                className="h-[300px] lg:h-[300px] mt-5 w-full"
+                config={chartConfig}
+              >
+                <BarChart
+                  data={chartData}
+                  layout="vertical"
+                  margin={{
+                    right: 16,
+                  }}
                 >
-                  <LabelList
-                    dataKey="paidPercentage"
-                    position="right"
-                    offset={8}
-                    className="fill-foreground"
-                    fontSize={12}
-                    formatter={(value) => `${value}%`}
+                  <CartesianGrid horizontal={false} />
+                  <YAxis
+                    dataKey="loanType"
+                    type="category"
+                    tickLine={false}
+                    axisLine={false}
                   />
-                </Bar>
-              </BarChart>
-            </ChartContainer>
-          </CardContent>
-        </Card>
-        <div className="border rounded-lg col-span-3 lg:col-auto border-gray-100 shadow-md"></div>
+                  <XAxis
+                    dataKey="paidPercentage"
+                    type="number"
+                    domain={[0, 100]}
+                    tickFormatter={(value) => `${value}%`}
+                  />
+                  <ChartTooltip cursor={false} content={<CustomTooltip />} />
+                  <Bar
+                    dataKey="paidPercentage"
+                    fill="var(--color-desktop)"
+                    radius={4}
+                  >
+                    <LabelList
+                      dataKey="paidPercentage"
+                      position="right"
+                      offset={8}
+                      className="fill-foreground"
+                      fontSize={12}
+                      formatter={(value) => `${value}%`}
+                    />
+                  </Bar>
+                </BarChart>
+              </ChartContainer>
+            </CardContent>
+          </Card>
+        </div>
+      </div>
+      <div className="border rounded-lg w-full col-span-1 border-gray-100 shadow-md h-full">
+        hello
       </div>
     </div>
   );
