d7680904d437c73a997b76fafab7b7d4131c1b86
SiriusShift <lagmanmarquez@gmail.com>
2024-11-08 16:34:21 +0800

    dashboard update

--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -25,7 +25,12 @@ import {
   PopoverTrigger,
 } from "@/components/ui/popover";
 
-import { Card, CardContent, CardDescription } from "@/components/ui/card";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardTitle,
+} from "@/components/ui/card";
 import {
   ChartConfig,
   ChartContainer,
@@ -36,64 +41,20 @@ import {
 export const description = "Loan Payment Progress Chart";
 
 const chartData = [
-  {
-    loanType: "Car Loan",
-    paidPercentage: 75,
-    paidAmount: 15000,
-    fill: "hsl(0, 0%, 61%)",
-  },
-  {
-    loanType: "House Loan",
-    paidPercentage: 50,
-    paidAmount: 50000,
-    fill: "hsl(0, 0%, 39%)",
-  },
-  {
-    loanType: "Laptop Loan",
-    paidPercentage: 40,
-    paidAmount: 2000,
-    fill: "hsl(0, 0%, 24%)",
-  },
-  {
-    loanType: "Property Loan",
-    paidPercentage: 30,
-    paidAmount: 30000,
-    fill: "hsl(0, 0%, 66%)",
-  },
-  {
-    loanType: "Other Loans",
-    paidPercentage: 20,
-    paidAmount: 500,
-    fill: "hsl(0, 0%, 67%)",
-  },
+  { expenseType: "Baby", running: 45 },
+  { expenseType: "Car", running: 50 },
+  { expenseType: "Car", running: 50 },
+  { expenseType: "Car", running: 23 },
+  { expenseType: "Car", running: 60 },
+  { expenseType: "Laptop", running: 60 },
 ];
 
 const chartConfig = {
-  paidPercentage: {
-    label: "Paid Percentage",
-  },
-  car: {
-    label: "Car Loan",
-    color: "hsl(0, 0%, 61%)",
-  },
-  house: {
-    label: "House Loan",
-    color: "hsl(0, 0%, 39%)",
-  },
-  laptop: {
-    label: "Laptop Loan",
-    color: "hsl(0, 0%, 24%)",
-  },
-  property: {
-    label: "Property Loan",
-    color: "hsl(0, 0%, 66%)",
-  },
-  other: {
-    label: "Other Loans",
-    color: "hsl(0, 0%, 67%)",
+  running: {
+    label: "Expense",
+    color: "hsl(0, 0%, 0%)", // Black color
   },
 } satisfies ChartConfig;
-
 const Dashboard = () => {
   const [date, setDate] = React.useState<Date>();
 
@@ -112,51 +73,13 @@ const Dashboard = () => {
   };
 
   return (
-    <div className="flex flex-col gap-5">
-      {/* <div className="flex items-center justify-between">
-        <h1 className="text-4xl lg:text-2xl font-bold">Dashboard</h1>
-        <Popover>
-          <PopoverTrigger asChild>
-            <Button
-              id="date"
-              variant={"outline"}
-              className={cn(
-                "w-[250px] justify-start text-left font-normal",
-                !date && "text-muted-foreground"
-              )}
-            >
-              <CalendarIcon className="mr-2" />
-              {date?.from ? (
-                date.to ? (
-                  <>
-                    {format(date.from, "LLL dd, y")} -{" "}
-                    {format(date.to, "LLL dd, y")}
-                  </>
-                ) : (
-                  format(date.from, "LLL dd, y")
-                )
-              ) : (
-                <span className="text-lg lg:text-base">Pick a date</span>
-              )}
-            </Button>
-          </PopoverTrigger>
-          <PopoverContent className="w-auto p-0" align="end">
-            <Calendar
-              initialFocus
-              mode="range"
-              defaultMonth={date?.from}
-              selected={date}
-              onSelect={setDate}
-              numberOfMonths={2}
-            />
-          </PopoverContent>
-        </Popover>
-      </div> */}
-      <div className="gap-5 grid  2xl:flex-row">
-        <div className="flex w-full flex-col gap-5">
-          <div className="grid grid-cols-4 md:grid-rows-1 lg:grid-rows-1 gap-5">
-            <Card className="border p-5 flex flex-col justify-between rounded-lg col-span-2 md:col-span-2 xl:col-span-1 h-36 shadow-md">
-              {/* <div className="flex justify-between">
+    <>
+      <div className="flex flex-col 2xl:flex-row gap-5">
+        <div className="gap-5 flex w-4/5 flex-col 2xl:flex-row">
+          <div className="flex w-full flex-col gap-5">
+            <div className="grid grid-cols-3 md:grid-rows-1 lg:grid-rows-1 gap-5">
+              <Card className="border p-5 flex flex-col justify-between rounded-lg xl:col-span-1 h-72 shadow-md">
+                {/* <div className="flex justify-between">
                 <div>
                   <h1 className="text-xl">Balance</h1>
                   <p className="text-sm text-gray-400">September 2024</p>
@@ -170,67 +93,57 @@ const Dashboard = () => {
                   ₱ 100,000
                 </h2>
               </div> */}
-            </Card>
-            <div className="border rounded-lg col-span-2 md:col-span-2 xl:col-span-1 h-36 shadow-md"></div>
-            <div className="border rounded-lg col-span-2 md:col-span-2 xl:col-span-1 h-36 shadow-md"></div>
-            <div className="border rounded-lg col-span-2 md:col-span-2 xl:col-span-1 h-36 shadow-md"></div>
-          </div>
-          <div className="gap-5 grid-rows-1 grid grid-cols-3">
-            <Card className="col-span-4  lg:col-span-3 border rounded-lg shadow-md">
-              <div className="mx-6 mt-6">
-                <h1 className="text-xl font-bold">Active Loans</h1>
-                <CardDescription className="text-base">
-                  Your next payment of ₱10,500 is scheduled for October 30, 2024
-                </CardDescription>
-              </div>
-              <CardContent>
-                <ChartContainer
-                  className="h-[300px] mt-5 w-full"
-                  config={chartConfig}
-                >
-                  <BarChart
-                    data={chartData}
-                    layout="vertical"
-                    margin={{
-                      right: 16,
-                    }}
-                  >
-                    <CartesianGrid horizontal={false} />
-                    <YAxis
-                      dataKey="loanType"
-                      type="category"
-                      tickLine={false}
-                      axisLine={false}
-                    />
-                    <XAxis
-                      dataKey="paidPercentage"
-                      type="number"
-                      domain={[0, 100]}
-                      tickFormatter={(value) => `${value}%`}
-                    />
-                    <ChartTooltip cursor={false} content={<CustomTooltip />} />
-                    <Bar
-                      dataKey="paidPercentage"
-                      fill="var(--color-desktop)"
-                      radius={4}
+              </Card>
+              <Card className="border rounded-lg col-span-2 md:col-span-2 xl:col-span-1 h-36 shadow-md"></Card>
+              <Card className="border rounded-lg col-span-2 md:col-span-2 xl:col-span-1 h-36 shadow-md"></Card>
+            </div>
+            <div className="gap-5 grid-rows-1 grid grid-cols-4">
+              <div className="col-span-2">
+                <Card className="p-0">
+                  <CardContent>
+                    <ChartContainer
+                      className="h-[250px] w-full"
+                      config={chartConfig}
                     >
-                      <LabelList
-                        dataKey="paidPercentage"
-                        position="right"
-                        offset={8}
-                        className="fill-foreground"
-                        fontSize={12}
-                        formatter={(value) => `${value}%`}
-                      />
-                    </Bar>
-                  </BarChart>
-                </ChartContainer>
-              </CardContent>
-            </Card>
+                      <BarChart accessibilityLayer data={chartData}>
+                        <XAxis
+                          dataKey="expenseType"
+                          tickLine={false}
+                          tickMargin={10}
+                          axisLine={false}
+                          tickFormatter={(value) => value}
+                        />
+                        <YAxis
+                          domain={[0, 100]} // Y-axis range from 0 to 100
+                          axisLine={false} // Remove the axis line
+                          tick={false} // Hide the ticks
+                          width={0}
+                        />
+                        {/* Set Y-axis range from 0 to 100 */}
+                        <Bar
+                          dataKey="running"
+                          stackId="a"
+                          fill="black" // Set bar color to black
+                          radius={[0, 0, 4, 4]}
+                          background={{ fill: "#eee" }}
+                        >
+
+                        </Bar>
+                        <ChartTooltip
+                          content={<ChartTooltipContent indicator="line" />}
+                          cursor={false}
+                          defaultIndex={1}
+                        />
+                      </BarChart>
+                    </ChartContainer>
+                  </CardContent>
+                </Card>
+              </div>
+            </div>
           </div>
         </div>
-        <div className="flex flex-row 2xl:flex-col gap-5">
-          <div className="w-1/2 xl:min-w-[350px] rounded lg p-5 border shadow-md ">
+        <div className="flex flex-row w-[350px] 2xl:flex-col gap-5">
+          <div className="rounded lg p-7 border shadow-md ">
             <div className="flex justify-between items-center">
               <h1 className="gap-3 text-xl font-semibold">
                 Recent Transactions
@@ -338,7 +251,7 @@ const Dashboard = () => {
               </div> */}
             </div>
           </div>
-          <div className="w-1/2 xl:min-w-[350px] rounded lg p-5 border shadow-md ">
+          <div className="rounded lg p-7 border shadow-md ">
             <div className="flex justify-between items-center">
               <h1 className="gap-3 text-xl font-semibold">
                 Recent Transactions
@@ -422,7 +335,7 @@ const Dashboard = () => {
             </div>
             <div className="flex mt-5 rounded-md justify-between">
               <div className="flex w-full">
-                <div className="p-2 border rounded-md bg-white flex justify-center shadow-md">
+                <div className="p-2 border rounded-md bg-white max-h-12 max-w-12 flex justify-center shadow-md">
                   <UtensilsCrossed
                     className="text-black"
                     width={25}
@@ -448,7 +361,7 @@ const Dashboard = () => {
           </div>
         </div>
       </div>
-    </div>
+    </>
   );
 };
 
